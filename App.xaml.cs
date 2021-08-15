5459bfb4ed397f140afba60d6ac4d97bae111beb
ptejam1984 <61092392+ptejam1984@users.noreply.github.com>
2021-08-15 20:23:13 +0100

    First ARC GIS app created using sample code

---
+++ App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FirstArcGISApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            // Note: it is not best practice to store API keys in source code.
+            // The API key is referenced here for the convenience of this tutorial.
+            Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.ApiKey = "AAPKee66d765df27402bbb4299c8dba6be73QT0UkCUjSjURaCjoHaadHkZQHUq5w0Psx7vI0P1V-c-wXz0slmzPqnQuuZ1fXIKJ";
+        }
+
+    }
+}
---
+++ DataManager.cs
@@ -0,0 +1,64 @@
+﻿// Copyright 2018 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+// language governing permissions and limitations under the License.
+#if !_SSG_TOOLING_
+using Esri.ArcGISRuntime.Portal;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FirstArcGISApp
+{
+    public static class DataManager
+    {
+        /// <summary>
+        /// Gets the data folder where locally provisioned data is stored.
+        /// </summary>
+        internal static string GetDataFolder()
+        {
+#if NETFX_CORE
+            string appDataFolder  = Windows.Storage.ApplicationData.Current.LocalFolder.Path;
+#elif XAMARIN
+            string appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+#else
+            string appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+#endif
+            string sampleDataFolder = Path.Combine(appDataFolder, "ArcGISRuntimeSampleData");
+
+            if (!Directory.Exists(sampleDataFolder)) { Directory.CreateDirectory(sampleDataFolder); }
+
+            return sampleDataFolder;
+        }
+
+        /// <summary>
+        /// Gets the path to an item on disk. 
+        /// The item must have already been downloaded for the path to be valid.
+        /// </summary>
+        /// <param name="itemId">ID of the portal item.</param>
+        internal static string GetDataFolder(string itemId)
+        {
+            return Path.Combine(GetDataFolder(), itemId);
+        }
+
+        /// <summary>
+        /// Gets the path to an item on disk. 
+        /// The item must have already been downloaded for the path to be valid.
+        /// </summary>
+        /// <param name="itemId">ID of the portal item.</param>
+        /// <param name="pathParts">Components of the path.</param>
+        internal static string GetDataFolder(string itemId, params string[] pathParts)
+        {
+            return Path.Combine(GetDataFolder(itemId), Path.Combine(pathParts));
+        }
+    }
+}
+#endif
